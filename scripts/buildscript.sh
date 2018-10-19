17a2399b307642448ece956f572972b66cb2de99
matteo.gianello <matteo.gianello@creactives.com>
2018-10-19 22:18:52 +0200

    change build script

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../src
-env GOOS=linux GOARCH=arm GOARM=5 go build commands.go gervasobot.go -o gervasobot
+env GOOS=linux GOARCH=arm GOARM=5 go build -o ../gervasobot commands.go gervasobot.go 
